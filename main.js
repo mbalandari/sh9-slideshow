ce46049c324ca2fc42e0ac68a5fad059657c734a
codingMaz <m.balandari.toroghi@gmail.com>
2023-06-26 14:06:29 +0330

    move slide to the next dynamically

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+var currentSlide = 0
+
 setInterval(function () {
 
-    $('.holder').css('left', '-100vw')
+    currentSlide = currentSlide + 1
+    var vwUnit = -currentSlide * 100
+    var leftPosition = vwUnit + 'vw'
+
+    $('.holder').css('left', leftPosition)
 
 }, 3000)
 
